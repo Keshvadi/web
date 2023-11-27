d5287e93026073f69e6f5668cadf65a53aa613bb
Sina Keshvadi <webidsina@gmail.com>
2023-11-27 12:24:25 -0800

    Updates

---
+++ precache-manifest.d791ef0e1a31da32cc104917b3a709cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d335722ddd14f8d581afae4d6fed3c9",
+    "url": "/Keshvadi/tictactoe/index.html"
+  },
+  {
+    "revision": "acd1e9de55e6d2906438",
+    "url": "/Keshvadi/tictactoe/static/css/main.9d171745.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/Keshvadi/tictactoe/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/Keshvadi/tictactoe/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "acd1e9de55e6d2906438",
+    "url": "/Keshvadi/tictactoe/static/js/main.43c1365e.chunk.js"
+  },
+  {
+    "revision": "f7df0c9c0798ba7c778f",
+    "url": "/Keshvadi/tictactoe/static/js/runtime~main.e4ed79f9.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Keshvadi/Web-Programming/precache-manifest.7a85fe4b93db1467a9484fa761e000cd.js"
+  "/Keshvadi/tictactoe/precache-manifest.d791ef0e1a31da32cc104917b3a709cf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Keshvadi/Web-Programming/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Keshvadi/tictactoe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
