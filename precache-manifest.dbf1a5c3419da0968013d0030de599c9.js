b8372ed18d4e3970a4297368375ed22beee6a2c8
Sina Keshvadi <webidsina@gmail.com>
2023-11-27 12:29:36 -0800

    Updates

---
+++ precache-manifest.dbf1a5c3419da0968013d0030de599c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4821c0d4e248898ae1cf4a45d8b3532a",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "acd1e9de55e6d2906438",
+    "url": "/tictactoe/static/css/main.9d171745.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/tictactoe/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/tictactoe/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "acd1e9de55e6d2906438",
+    "url": "/tictactoe/static/js/main.43c1365e.chunk.js"
+  },
+  {
+    "revision": "cd6d73f9c58cb9111be3",
+    "url": "/tictactoe/static/js/runtime~main.44516421.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Keshvadi/tictactoe/precache-manifest.d791ef0e1a31da32cc104917b3a709cf.js"
+  "/tictactoe/precache-manifest.dbf1a5c3419da0968013d0030de599c9.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Keshvadi/tictactoe/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tictactoe/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
