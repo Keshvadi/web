7c61b983c862e941fd956b85cdc8858f4be34ff4
Sina Keshvadi <webidsina@gmail.com>
2023-11-27 11:30:52 -0800

    Updates

---
+++ precache-manifest.7a85fe4b93db1467a9484fa761e000cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f0376ed90ffb120b4385d42ee0014dc",
+    "url": "/Keshvadi/Web-Programming/index.html"
+  },
+  {
+    "revision": "acd1e9de55e6d2906438",
+    "url": "/Keshvadi/Web-Programming/static/css/main.9d171745.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/Keshvadi/Web-Programming/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/Keshvadi/Web-Programming/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "acd1e9de55e6d2906438",
+    "url": "/Keshvadi/Web-Programming/static/js/main.43c1365e.chunk.js"
+  },
+  {
+    "revision": "a5ed53f819ff7ef3c0e9",
+    "url": "/Keshvadi/Web-Programming/static/js/runtime~main.ab5d876f.js"
+  }
+]);
